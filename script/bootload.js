0d83d93bd2ac3cc2cc912c0c80de9997d3ff2470
mathieu juglet <mathieujuglet@MBPdeMathieu2.wifi.froment.in.iesanetwork.com>
2016-01-27 16:45:55 +0100

    ++

--- script/bootload.js
+++ script/bootload.js
@@ -1,12 +1,21 @@
-object.onkeydown=function(event){
-var keycode;
-   keycode = event.keyCode;
+
+
+document.body.onkeydown = function(event) {
+
+	event = event || window.event;
+
+ 	var keycode = event.keyCode,
+  	keycode = event.keyCode,
+  	car = document.getElementById("car");
 	
-if (keycode == 40)<!-- aller en bas -->
-else if (keycode == 39)<!-- aller a droite -->
-else if (keycode == 37) <!-- aller a gauche -->
-else if (keycode == 38) <!-- aller en haut -->
-	
+	if (keycode == 40){<!-- aller en bas -->
+		car.className = "car-to-bottom";
+	}else if (keycode == 39){<!-- aller a droite -->
+		car.className = "car-to-right";
+	}else if (keycode == 37){ <!-- aller a gauche -->
+		car.className = "car-to-left";
+	}else if (keycode == 38){ <!-- aller en haut -->
+		car.className = "car-to-top";
+	}
+
 };
-
-
