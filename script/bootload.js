e9fd0e424a65a2df409e2603130682cc1da3adfd
mathieu juglet <mathieujuglet@MBPdeMathieu2.wifi.froment.in.iesanetwork.com>
2016-01-27 17:50:51 +0100

    THIS CAR WORK

--- script/bootload.js
+++ script/bootload.js
@@ -1,24 +1,24 @@
 document.body.onkeydown = function(event) {
 
+
+
     event = event || window.event;
 
     var keycode = event.keyCode,
-        keycode = event.keyCode,
         car = document.getElementById('car'),
     	lastClassName = car.className;
     	car.className = "car-transition";
 
-	console.log(lastClassName);
-
     if (keycode == 40) {
-    	if( lastClassName == "car-transition car-to-left" ){
+    	if( lastClassName == "car-transition car-to-left"  || lastClassName == "car-to-left"){
 
     		car.classList.add("car-to-bottom-reverse");	
     		setTimeout(function(){
     			car.className = ""
     			car.classList.add("car-to-bottom");
+
     		}, 1000);
-    		
+
     	}else{
     		car.classList.add("car-to-bottom");	
     	}
@@ -28,23 +28,22 @@ document.body.onkeydown = function(event) {
     	car.classList.add("car-to-right");	
         
     } else if (keycode == 37) {
-    	if( lastClassName == "car-transition car-to-bottom" ){
+
+    	if( lastClassName == "car-transition car-to-bottom" || lastClassName == "car-to-bottom"){
     		car.classList.add("car-to-final");
     		setTimeout(function(){
     			car.className = ""
-    			// car.classList.add("car-transition");
     			car.classList.add("car-to-left");
     			
     		}, 1000);
     	}else{
         	car.classList.add("car-to-left");
         }
+
     } else if (keycode == 38) {
-    	// if( lastClassName == "car-to-right" ){
-    	// 	car.classList.add("car-to-final");
-    	// }else{
-        	car.classList.add("car-to-top");
-        // }
+
+        car.classList.add("car-to-top");
+        
     }
 
 };
