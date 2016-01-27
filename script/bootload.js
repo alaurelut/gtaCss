0b78228f46ae13dfea32b8fe26fd46dddb54bdd3
alaurelut <axel.laurelut@cifacom.com>
2016-01-27 16:26:56 +0100

    add keycode events

--- script/bootload.js
+++ script/bootload.js
@@ -1,5 +1,12 @@
-object.onkeydown=function(){
-
+object.onkeydown=function(event){
+var keycode;
+   keycode = event.keyCode;
 	
+if (keycode == 40)<!-- aller en bas -->
+else if (keycode == 39)<!-- aller a droite -->
+else if (keycode == 37) <!-- aller a gauche -->
+else if (keycode == 38) <!-- aller en haut -->
 	
 };
+
+
