9f879a5ad3ff8a67a0dd228106d683bb7ea72f8a
alaurelut <axel.laurelut@cifacom.com>
2016-02-08 17:59:16 +0100

    final commit car moving + 2 more cars

--- script/bootload.js
+++ script/bootload.js
@@ -1,6 +1,9 @@
+var player = document.getElementById('car');
+var acceleration = 10;
+player.style.top = '320px';
+player.style.left = '200px';
+
 document.body.onkeydown = function(event) {
-
-
 
     event = event || window.event;
 
@@ -10,6 +13,11 @@ document.body.onkeydown = function(event) {
     	car.className = "car-transition";
 
     if (keycode == 40) {
+
+        player.style.top = parseInt(player.style.top) + acceleration + 'px';
+        window.scrollTo(window.pageXOffset, window.pageYOffset + 20);
+
+
     	if( lastClassName == "car-transition car-to-left"  || lastClassName == "car-to-left"){
 
     		car.classList.add("car-to-bottom-reverse");	
@@ -21,12 +29,17 @@ document.body.onkeydown = function(event) {
     	}else{
     		car.classList.add("car-to-bottom");	
     	}
-        
+
     } else if (keycode == 39) {
-    	
-    	car.classList.add("car-to-right");	
+
+        player.style.left = parseInt(player.style.left) + acceleration + 'px';
+        window.scrollTo(window.pageXOffset + 20, window.pageYOffset);
+    	car.classList.add("car-to-right");
         
     } else if (keycode == 37) {
+
+        player.style.left = parseInt(player.style.left) - acceleration + 'px';
+        window.scrollTo(window.pageXOffset - 20, window.pageYOffset);
 
     	if( lastClassName == "car-transition car-to-bottom" || lastClassName == "car-to-bottom"){
     		car.classList.add("car-to-final");
@@ -38,10 +51,13 @@ document.body.onkeydown = function(event) {
         	car.classList.add("car-to-left");
         }
 
+        
+
     } else if (keycode == 38) {
 
+        player.style.top = parseInt(player.style.top) - acceleration + 'px';
+        window.scrollTo(window.pageXOffset, window.pageYOffset - 20);
         car.classList.add("car-to-top");
-
     }
 
 };
